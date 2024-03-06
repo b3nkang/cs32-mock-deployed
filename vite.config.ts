c1455e00f6ed620ee48618e4a7ca62ddb9155378
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-03-05 23:02:43 -0500

    fix: add base for config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
+    base: "/cs32-mock-deployed/",
     build: {
-      outDir: 'build',
+      outDir: "build",
     },
     plugins: [react()],
     server: {
